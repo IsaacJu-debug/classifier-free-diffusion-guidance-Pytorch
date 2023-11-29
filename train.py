61b218e3e638861b0099dbe08fac47821fcb6969
IsaacJu-debug <ju1@stanford.edu>
2023-11-29 14:39:42 -0800

    Initial commit for serial version

--- train.py
+++ train.py
@@ -2,8 +2,8 @@ import os
 import torch
 import argparse
 import itertools
-import numpy as np
 from unet import Unet
+import numpy as np
 from tqdm import tqdm
 import torch.optim as optim
 from diffusion import GaussianDiffusion
@@ -12,32 +12,26 @@ from utils import get_named_beta_schedule
 from embedding import ConditionalEmbedding
 from Scheduler import GradualWarmupScheduler
 from dataloader_cifar import load_data, transback
-from torch.nn.parallel import DistributedDataParallel as DDP
-from torch.distributed import get_rank, init_process_group, destroy_process_group, all_gather, get_world_size
 
-def train(params:argparse.Namespace):
-    assert params.genbatch % (torch.cuda.device_count() * params.clsnum) == 0 , 'please re-set your genbatch!!!'
-    # initialize settings
-    init_process_group(backend="nccl")
-    # get local rank for each process
-    local_rank = get_rank()
-    # set device
-    device = torch.device("cuda", local_rank)
-    # load data
-    dataloader, sampler = load_data(params.batchsize, params.numworkers)
-    # initialize models
+def train(params: argparse.Namespace):
+    # Set device for single GPU
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # Load data
+    dataloader = load_data(params.batchsize, params.numworkers)
+    # Initialize models
     net = Unet(
-                in_ch = params.inch,
-                mod_ch = params.modch,
-                out_ch = params.outch,
-                ch_mul = params.chmul,
-                num_res_blocks = params.numres,
-                cdim = params.cdim,
-                use_conv = params.useconv,
-                droprate = params.droprate,
-                dtype = params.dtype
-            )
+        in_ch=params.inch,
+        mod_ch=params.modch,
+        out_ch=params.outch,
+        ch_mul=params.chmul,
+        num_res_blocks=params.numres,
+        cdim=params.cdim,
+        use_conv=params.useconv,
+        droprate=params.droprate,
+        dtype=params.dtype
+    ).to(device)
     cemblayer = ConditionalEmbedding(10, params.cdim, params.cdim).to(device)
+    
     # load last epoch
     lastpath = os.path.join(params.moddir,'last_epoch.pt')
     if os.path.exists(lastpath):
@@ -56,18 +50,6 @@ def train(params:argparse.Namespace):
                     w = params.w,
                     v = params.v,
                     device = device
-                )
-    
-    # DDP settings 
-    diffusion.model = DDP(
-                            diffusion.model,
-                            device_ids = [local_rank],
-                            output_device = local_rank
-                        )
-    cemblayer = DDP(
-                    cemblayer,
-                    device_ids = [local_rank],
-                    output_device = local_rank
                 )
     # optimizer settings
     optimizer = torch.optim.AdamW(
@@ -95,74 +77,80 @@ def train(params:argparse.Namespace):
     if lastepc != 0:
         optimizer.load_state_dict(checkpoint['optimizer'])
         warmUpScheduler.load_state_dict(checkpoint['scheduler'])
-    # training
-    cnt = torch.cuda.device_count()
+
+    # training process
     for epc in range(lastepc, params.epoch):
-        # turn into train mode
-        diffusion.model.train()
+        # Turn into train mode
+        net.train()
         cemblayer.train()
-        sampler.set_epoch(epc)
-        # batch iterations
-        with tqdm(dataloader, dynamic_ncols=True, disable=(local_rank % cnt != 0)) as tqdmDataLoader:
+
+        # Batch iterations
+        with tqdm(dataloader, dynamic_ncols=True) as tqdmDataLoader:
             for img, lab in tqdmDataLoader:
                 b = img.shape[0]
                 optimizer.zero_grad()
+
+                # Move data to the device
                 x_0 = img.to(device)
                 lab = lab.to(device)
+
+                # Get conditional embedding
                 cemb = cemblayer(lab)
-                cemb[np.where(np.random.rand(b)<params.threshold)] = 0
-                loss = diffusion.trainloss(x_0, cemb = cemb)
+                cemb[np.where(np.random.rand(b) < params.threshold)] = 0
+
+                # Compute loss and backpropagate
+                loss = diffusion.trainloss(x_0, cemb=cemb)
                 loss.backward()
                 optimizer.step()
+
+                # Update progress bar
                 tqdmDataLoader.set_postfix(
                     ordered_dict={
                         "epoch": epc + 1,
                         "loss: ": loss.item(),
-                        "batch per device: ":x_0.shape[0],
+                        "batch per device: ": x_0.shape[0],
                         "img shape: ": x_0.shape[1:],
                         "LR": optimizer.state_dict()['param_groups'][0]["lr"]
                     }
                 )
+
+        # Scheduler step
         warmUpScheduler.step()
-        # evaluation and save checkpoint
+
+        # Evaluation and save checkpoint
         if (epc + 1) % params.interval == 0:
-            diffusion.model.eval()
+            net.eval()
             cemblayer.eval()
-            # generating samples
-            # The model generate 80 pictures(8 per row) each time
-            # pictures of same row belong to the same class
-            all_samples = []
-            each_device_batch = params.genbatch // cnt
+
+            # Generating samples
             with torch.no_grad():
-                lab = torch.ones(params.clsnum, each_device_batch // params.clsnum).type(torch.long) \
-                * torch.arange(start = 0, end = params.clsnum).reshape(-1, 1)
-                lab = lab.reshape(-1, 1).squeeze()
-                lab = lab.to(device)
+                lab = torch.arange(start=0, end=params.clsnum).repeat(params.genbatch // params.clsnum).to(device)
                 cemb = cemblayer(lab)
-                genshape = (each_device_batch , 3, 32, 32)
+                genshape = (params.genbatch, 3, 32, 32)
+
+                # Sample generation
                 if params.ddim:
-                    generated = diffusion.ddim_sample(genshape, params.num_steps, params.eta, params.select, cemb = cemb)
+                    generated = diffusion.ddim_sample(genshape, params.num_steps, params.eta, params.select, cemb=cemb)
                 else:
-                    generated = diffusion.sample(genshape, cemb = cemb)
+                    generated = diffusion.sample(genshape, cemb=cemb)
+
+                # Convert and save images
                 img = transback(generated)
-                img = img.reshape(params.clsnum, each_device_batch // params.clsnum, 3, 32, 32).contiguous()
-                gathered_samples = [torch.zeros_like(img) for _ in range(get_world_size())]
-                all_gather(gathered_samples, img)
-                all_samples.extend([img for img in gathered_samples])
-                samples = torch.concat(all_samples, dim = 1).reshape(params.genbatch, 3, 32, 32)
-                if local_rank == 0:
-                    save_image(samples, os.path.join(params.samdir, f'generated_{epc+1}_pict.png'), nrow = params.genbatch // params.clsnum)
-            # save checkpoints
+                save_image(img, os.path.join(params.samdir, f'generated_{epc+1}_pict.png'), nrow=params.genbatch // params.clsnum)
+
+            # Save checkpoints
             checkpoint = {
-                                'net':diffusion.model.module.state_dict(),
-                                'cemblayer':cemblayer.module.state_dict(),
-                                'optimizer':optimizer.state_dict(),
-                                'scheduler':warmUpScheduler.state_dict()
-                            }
-            torch.save({'last_epoch':epc+1}, os.path.join(params.moddir,'last_epoch.pt'))
+                'net': net.state_dict(),
+                'cemblayer': cemblayer.state_dict(),
+                'optimizer': optimizer.state_dict(),
+                'scheduler': warmUpScheduler.state_dict()
+            }
+            torch.save({'last_epoch': epc + 1}, os.path.join(params.moddir, 'last_epoch.pt'))
             torch.save(checkpoint, os.path.join(params.moddir, f'ckpt_{epc+1}_checkpoint.pt'))
+
+        # Clear CUDA cache
         torch.cuda.empty_cache()
-    destroy_process_group()
+
 
 def main():
     # several hyperparameters for model
@@ -195,7 +183,7 @@ def main():
     parser.add_argument('--eta',type=float,default=0,help='eta for variance during DDIM sampling process')
     parser.add_argument('--select',type=str,default='linear',help='selection stragies for DDIM')
     parser.add_argument('--ddim',type=lambda x:(str(x).lower() in ['true','1', 'yes']),default=False,help='whether to use ddim')
-    parser.add_argument('--local_rank',default=-1,type=int,help='node rank for distributed training')
+    #parser.add_argument('--local_rank',default=-1,type=int,help='node rank for distributed training')
     
     args = parser.parse_args()
     train(args)
